9b5c3b6cdecadd5c7d8f44d9d88ecd2d8a307093
Your Name <you@example.com>
2025-04-07 00:34:05 +0530

    added everything ig

--- frontend/src/layouts/authentication/logout/index.js
+++ frontend/src/layouts/authentication/logout/index.js
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signOut } from 'services/auth'; // Replace with your logout service
+// import { signOut } from 'services/auth'; // Replace with your logout service
 
 function Logout() {
   const navigate = useNavigate();
 
   useEffect(() => {
     // Perform logout operations
-    signOut();
+    // signOut();
     navigate('/authentication/sign-in');
   }, [navigate]);
 
